f652ff29e3e4fb680f68c46b931cead4a2ab4193
AdrianAdrianAdrian <71560208+Adrian-Kosnik@users.noreply.github.com>
2024-06-06 09:47:19 +0100

    updates

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    with open('books/frankenstein.txt') as f:
+        file_contents = f.read()
+        # print(file_contents)
+        words = file_contents.split()
+        print(len(words))
+
+def letter_count(book):
+    dic = {}
+    low_book = book.lower()
+
+    for letter in book:
+        if 
+    
+
+
+main()
