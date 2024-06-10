b364636b5dcc908a48f7d7d3467857ac0ceaeba4
AdrianAdrianAdrian <71560208+Adrian-Kosnik@users.noreply.github.com>
2024-06-10 20:05:27 +0100

    got the dic together

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+def letter_count(book):
+    dic = {}
+    low_book = book.lower()
+    character_list = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z' , 'x', 'c', 'v', 'b', 'n', 'm']
+
+    for letter in low_book:
+        if letter in character_list:
+            if letter not in dic:
+                dic.update({letter: 1})
+            else:
+                dic[letter] += 1
+
+    return dic
+
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
         # print(file_contents)
         words = file_contents.split()
         print(len(words))
-
-def letter_count(book):
-    dic = {}
-    low_book = book.lower()
-
-    for letter in book:
-        if 
-    
-
+        print(letter_count(str(words)))
 
 main()
