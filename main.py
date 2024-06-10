b1a05a38ecc78c4629ee6a24cba4963e4485bd74
AdrianAdrianAdrian <71560208+Adrian-Kosnik@users.noreply.github.com>
2024-06-10 20:25:46 +0100

    updates

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-def letter_count(book):
-    dic = {}
-    low_book = book.lower()
-    character_list = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z' , 'x', 'c', 'v', 'b', 'n', 'm']
-
-    for letter in low_book:
-        if letter in character_list:
-            if letter not in dic:
-                dic.update({letter: 1})
-            else:
-                dic[letter] += 1
-
-    return dic
-
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-        # print(file_contents)
-        words = file_contents.split()
-        print(len(words))
-        print(letter_count(str(words)))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
